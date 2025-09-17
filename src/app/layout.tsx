7fadb28c85c32394074ca49663f60db1f606bcad
Polat Zafarov <atuin59354081@gmail.com>
2025-09-17 15:39:38 +0000

    измени фон на более интересный и цветовую палитру сайта на более строгий

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
       </head>
       <body
         className={cn(
-          'font-body antialiased',
+          'font-body antialiased animated-background',
           fontBody.variable,
           fontCode.variable
         )}
